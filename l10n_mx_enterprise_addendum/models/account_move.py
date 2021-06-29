5d839d6d54fc032c04d2c6a6e4e1d5ade0e64f4c
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-28 23:09:22 -0500

    Update account_move.py

--- l10n_mx_enterprise_addendum/models/account_move.py
+++ l10n_mx_enterprise_addendum/models/account_move.py
@@ -6,6 +6,6 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super(AccountMove, self).action_post()
-        if self.type in ("out_invoice", "out_refund") and not self.addendum_id.manual:
+        if self.move_type in ("out_invoice", "out_refund") and not self.addendum_id.manual:
             self.generate_addendum(raise_if_not_attachment=False)
         return res
