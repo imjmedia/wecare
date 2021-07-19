14e7505d88cf66b7bd4f27c89ff7be5c3f0128b8
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-07-18 23:56:55 -0500

    Creacion de Automatización CRM

---
+++ crm_modificado/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Generación Automatica de Revenue Estimado en CRM",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "IT Reingenierias S de RL",
+    'website': "http://www.itreingenierias.com",
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['crm'],
+}
