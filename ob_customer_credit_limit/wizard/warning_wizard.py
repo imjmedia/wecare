b5550c964d2d4e564b7cdd9333b865f40d3d68b5
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-08-20 08:26:17 -0500

    Línea de Credito

---
+++ ob_customer_credit_limit/wizard/warning_wizard.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class WarningWizard(models.TransientModel):
+    _name = "warning.wizard"
+    _description = "Warning Wizard"
+
+    def get_default(self):
+        if self.env.context.get("message", False):
+            return self.env.context.get("message")
+        return False
+
+    name = fields.Text(string="Message", readonly=True, default=get_default)
+    sale_id = fields.Many2one('sale.order', string="Sale Order")
+
+    def action_confirm(self):
+        self.sale_id.with_context(warning=True).action_confirm()
