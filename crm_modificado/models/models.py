49f710edee1eea9dcbacde88e07e0b3acb192495
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-08-31 13:43:48 -0500

    Update models.py

--- crm_modificado/models/models.py
+++ crm_modificado/models/models.py
@@ -5,14 +5,13 @@ from odoo import models, fields, api
 class crmModificado(models.Model):
     _inherit = 'crm.lead'
 
-    expected_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', tracking=True,compute="sacar_ingreso",store=True)
-
-    @api.depends('quotation_count','sale_amount_total')
+    @api.depends('quotation_count', 'sale_amount_total')
     def sacar_ingreso(self):
         self.expected_revenue = 0.0
         x = 0
         for record in self:
-            vendido = self.env['sale.order'].search([('opportunity_id', '=', record.id), ('state', '!=', 'sale'), ('state', '!=', 'cancel')])
+            vendido = self.env['sale.order'].search(
+                [('opportunity_id', '=', record.id), ('state', '!=', 'sale'), ('state', '!=', 'cancel')])
             if record.quotation_count == 0:
                 if record.sale_amount_total:
                     record.expected_revenue = record.sale_amount_total
@@ -28,3 +27,6 @@ class crmModificado(models.Model):
                         x = x + 1
             else:
                 record.expected_revenue = 0.0
+
+    expected_revenue = fields.Monetary('Expected Revenue', currency_field='company_currency', tracking=True,compute="sacar_ingreso",store=True)
+
