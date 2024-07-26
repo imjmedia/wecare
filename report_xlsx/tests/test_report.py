b8cf58449ace261e9e1e7c612619d27df8e3fcf9
unknown <fmanime@gmail.com>
2024-07-26 15:55:11 -0600

    [MIG][report xlsx, report xlsx helper] Actualizado a su v17

--- report_xlsx/tests/test_report.py
+++ report_xlsx/tests/test_report.py
@@ -16,24 +16,32 @@ except ImportError:
 class TestReport(common.TransactionCase):
     def setUp(self):
         super().setUp()
-        report_object = self.env["ir.actions.report"]
+        self.report_object = self.env["ir.actions.report"]
         self.xlsx_report = self.env["report.report_xlsx.abstract"].with_context(
             active_model="res.partner"
         )
         self.report_name = "report_xlsx.partner_xlsx"
-        self.report = report_object._get_report_from_name(self.report_name)
+        self.report = self.report_object._get_report_from_name(self.report_name)
         self.docs = self.env["res.company"].search([], limit=1).partner_id
 
     def test_report(self):
         report = self.report
         self.assertEqual(report.report_type, "xlsx")
-        rep = report._render(self.docs.ids, {})
+        rep = self.report_object._render(self.report_name, self.docs.ids, {})
         wb = open_workbook(file_contents=rep[0])
         sheet = wb.sheet_by_index(0)
         self.assertEqual(sheet.cell(0, 0).value, self.docs.name)
 
-    def test_id_retrieval(self):
+    def test_save_attachment(self):
+        self.report.attachment = 'object.name + ".xlsx"'
+        self.report_object._render(self.report_name, self.docs.ids, {})
+        attachment = self.env["ir.attachment"].search(
+            [("res_id", "=", self.docs.id), ("res_model", "=", self.docs._name)]
+        )
+        self.assertEqual(len(attachment), 1)
+        self.assertEqual(attachment.name, f"{self.docs.name}.xlsx")
 
+    def test_id_retrieval(self):
         # Typical call from WebUI with wizard
         objs = self.xlsx_report._get_objs_for_report(
             False, {"context": {"active_ids": self.docs.ids}}
