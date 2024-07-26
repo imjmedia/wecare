e23019c183a088e1dcb7b7ad786df9d458451f64
unknown <fmanime@gmail.com>
2024-07-26 15:31:46 -0600

    [MIG][ob customer credit limit] Se migra a v17

--- l10n_mx_enterprise_addendum/__manifest__.py
+++ l10n_mx_enterprise_addendum/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "L10N MX Enterpise Addendum",
-    "version": "13.0.1.0.0",
+    "version": "17.0.1.0",
     "author": "HomebrewSoft",
     "website": "https://homebrewsoft.dev",
     "license": "LGPL-3",
@@ -9,10 +9,6 @@
         "l10n_mx_edi",
     ],
     "data": [
-        # security
-        # data
-        # reports
-        # views
         "views/res_partner.xml",
     ],
     "auto_install": True,
