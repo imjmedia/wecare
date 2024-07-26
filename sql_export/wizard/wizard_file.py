e69d084d26df2e2d6245abfc29ecd3592a81c28d
unknown <fmanime@gmail.com>
2024-07-26 15:41:20 -0600

    [MIG][sql export, excel y sql request abstract] Actualizado a su v17

--- sql_export/wizard/wizard_file.py
+++ sql_export/wizard/wizard_file.py
@@ -2,12 +2,10 @@
 # @author: Florian da Costa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import json
 from datetime import datetime
 
-from lxml import etree
-
-from odoo import api, fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
@@ -18,57 +16,40 @@ class SqlFileWizard(models.TransientModel):
     binary_file = fields.Binary("File", readonly=True)
     file_name = fields.Char(readonly=True)
     sql_export_id = fields.Many2one(comodel_name="sql.export", required=True)
-
-    @api.model
-    def fields_view_get(
-        self, view_id=None, view_type="form", toolbar=False, submenu=False
-    ):
-        """
-        Display dynamically parameter fields depending on the sql_export.
-        """
-        res = super(SqlFileWizard, self).fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
-        )
-        export_obj = self.env["sql.export"]
-        if view_type == "form":
-            sql_export = export_obj.browse(self.env.context.get("active_id"))
-            if sql_export.field_ids:
-                eview = etree.fromstring(res["arch"])
-                group = etree.Element("group", name="variables_group", colspan="4")
-                toupdate_fields = []
-                for field in sql_export.field_ids:
-                    toupdate_fields.append(field.name)
-                    attrib = {"name": field.name, "required": "0", "readonly": "0"}
-                    view_field = etree.SubElement(group, "field", attrib=attrib)
-                    modifiers = json.loads(view_field.get("modifiers", "{}"))
-                    if field.required:
-                        modifiers["required"] = True
-                    view_field.set("modifiers", json.dumps(modifiers))
-
-                res["fields"].update(self.fields_get(toupdate_fields))
-                placeholder = eview.xpath(
-                    "//separator[@string='variables_placeholder']"
-                )[0]
-                placeholder.getparent().replace(placeholder, group)
-                res["arch"] = etree.tostring(eview, pretty_print=True)
-        return res
+    query_properties = fields.Properties(
+        string="Properties",
+        definition="sql_export_id.query_properties_definition",
+        copy=False,
+    )
 
     def export_sql(self):
         self.ensure_one()
+
+        # Check if query_properties is a valid list
+        # if not self.query_properties or not isinstance(self.query_properties, list):
+            # self.query_properties = []
+
+        # # Check properties
+        # if not self.query_properties:
+            # self.query_properties = []
+        # bad_props = [x for x in self.query_properties if not x["value"]]
+        # if bad_props:
+            # raise UserError(
+                # _("Please enter a value for the following properties: %s")
+                # % (", ".join([x["string"] for x in bad_props]))
+            # )
+
         sql_export = self.sql_export_id
 
         # Manage Params
         variable_dict = {}
         now_tz = fields.Datetime.context_timestamp(sql_export, datetime.now())
         date = now_tz.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        if sql_export.field_ids:
-            for field in sql_export.field_ids:
-                if field.ttype == "many2one":
-                    variable_dict[field.name] = self[field.name].id
-                elif field.ttype == "many2many":
-                    variable_dict[field.name] = tuple(self[field.name].ids)
-                else:
-                    variable_dict[field.name] = self[field.name]
+        # for prop in self.query_properties:
+            # if prop["type"] == "many2many":
+                # variable_dict[prop["string"]] = tuple(prop["value"])
+            # else:
+                # variable_dict[prop["string"]] = prop["value"]
         if "%(company_id)s" in sql_export.query:
             company_id = self.env.company.id
             variable_dict["company_id"] = company_id
@@ -76,8 +57,7 @@ class SqlFileWizard(models.TransientModel):
             user_id = self.env.user.id
             variable_dict["user_id"] = user_id
 
-        # Call different method depending on file_type since the logic will be
-        # different
+        # Call different method depending on file_type since the logic will be different
         method_name = "%s_get_data_from_query" % sql_export.file_format
         data = getattr(sql_export, method_name)(variable_dict)
         extension = sql_export._get_file_extension()
@@ -88,12 +68,26 @@ class SqlFileWizard(models.TransientModel):
                 % {"name": sql_export.name, "date": date, "extension": extension},
             }
         )
-        return {
-            "view_mode": "form",
-            "res_model": "sql.file.wizard",
-            "res_id": self.id,
-            "type": "ir.actions.act_window",
-            "target": "new",
-            "context": self.env.context,
-            "nodestroy": True,
+        # Bypass ORM to avoid changing the write_date/uid from sql query on a simple execution.
+        # This also avoids error if user has no update right on the sql.export object.
+        self.env.cr.execute(
+            """
+            UPDATE sql_export
+            SET last_execution_date = %s, last_execution_uid = %s
+            WHERE id = %s
+        """,
+            (
+                fields.Datetime.to_string(fields.Datetime.now()),
+                self.env.user.id,
+                sql_export.id,
+            ),
+        )
+        action = {
+            "name": "SQL Export",
+            "type": "ir.actions.act_url",
+            "url": "web/content/?model=%s&id=%d&filename_field=filename&"
+            "field=binary_file&download=true&filename=%s"
+            % (self._name, self.id, self.file_name),
+            "target": "self",
         }
+        return action
