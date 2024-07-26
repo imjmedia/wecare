443b4390e77bb5fa74538d59027a89cb0664f4e6
unknown <fmanime@gmail.com>
2024-07-25 18:51:41 -0600

    [MIG][commisions] Se migra a v17

--- commissions/models/__init__.py
+++ commissions/models/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from . import res_users
 from . import sale_order
 from . import sale_commission
--- commissions/models/res_partner.py
+++ commissions/models/res_partner.py
@@ -2,7 +2,7 @@
 from odoo import api, fields, models, _
 
 
-class SmartCommissionPartner(models.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     partner_corp_id=fields.Many2one('res.partner', string='Corportive Company', index=True)
